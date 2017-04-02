6da8855dbb50f267e89c4edebe42ecc6b4e89de7
brad winter <braddjwinter@gmail.com>
2017-04-02 13:34:46 -0700

    domains

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 noisefloor.ca
 cyberspa.biz
 victorysquareblockparty.com
+
+# to add a site, add to 'domains' array in index.js, then add a folder containing site assets in sites/domain
--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-//var express = require('express');
 
-// require diet
-var server = require('diet');
-	
-// create an instance
-var app = server();
+var domains = [
+  'cyberspa.biz',
+  'noisefloor.ca',
+  'bradwinter.local'
+];
 
-console.log('port is '+process.env.PORT);
+var port = process.env.PORT || '8000';
+
+var connect = require('connect'),
+	serveStatic = require('serve-static'),
+	vhost = require('vhost'),
+ 	_ = require('lodash');
+
+
+var sites = {};
+_.each(domains, function(domain){
+    sites[domain] = connect();
+  })
+
+var app = connect();
+
+_.each(domains, function(domain){
+    sites[domain] = connect();
+    sites[domain].use(serveStatic('./sites/'+domain));
+    app.use(vhost(domain, sites[domain]));
+  })
+
+
+app.listen(port);
+
